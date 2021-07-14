423e3907c790af2cb94df1da84912b2de067942b
sb7297 <sbaireddy7297@gmail.com>
2021-07-14 13:42:17 -0400

    allow user to decrement date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import NasaMedia from "./components/NasaMedia";
@@ -7,19 +9,30 @@ import Title from "./components/Title";
 function App() {
   const [mediaUrl, setMediaUrl] = useState("");
   const [mediaType, setMediaType] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState("2021-07-14");
+
+  dayjs.extend(customParseFormat);
+  
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=bIUvX7E3fljMaeRzCp0Fp8HhHbLtIXnFJmTAE3p6&date=2021-07-14")
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=bIUvX7E3fljMaeRzCp0Fp8HhHbLtIXnFJmTAE3p6&date=${date}`)
     .then(res => {
       setMediaUrl(res.data.url);
       setDate(res.data.date);
       setMediaType(res.data.media_type);
     })
     .catch(err => console.log(err));
-  }, []);
+  }, [date]);
+
+  const decDate = () => {
+    const day = dayjs(date, 'YYYY-MM-DD');
+    const dayMinusOne = day.subtract(1, 'day');
+    const newDate = dayMinusOne.format('YYYY-MM-DD');
+    setDate(newDate);
+  };
+
   return (
     <div className="App">
-      <Title date={date} />
+      <Title date={date} decDate={decDate} />
       <NasaMedia url={mediaUrl} type={mediaType} />
     </div>
   );
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function Title({ date }) {
-    return <h1>{date}</h1>;
+function Title({ date, decDate }) {
+  const arrowStyle = {
+    cursor: "pointer"
+  };
+  return (
+    <div className="titlebar">
+      <h1>
+        <span style={arrowStyle} onClick={decDate}>⬅️ </span>
+        {date}
+        <span style={arrowStyle}> ➡️</span>
+      </h1>
+    </div>
+  );
 }
 
 export default Title;
