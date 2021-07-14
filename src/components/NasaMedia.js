98f7859190e522393bae58c4a345d0ea33707281
sb7297 <sbaireddy7297@gmail.com>
2021-07-14 12:45:57 -0400

    NasaMedia: style image to always fit on screen

--- src/components/NasaMedia.js
+++ src/components/NasaMedia.js
@@ -3,7 +3,11 @@ import React from "react";
 function NasaMedia(props) {
     const { url } = props;
     console.log(url);
-    return <img src={url} />;
+    const style = {
+        maxHeight: "84vh",
+        maxWidth: "90%",
+    };
+    return <img src={url} style={style} />;
 }
 
 export default NasaMedia;
