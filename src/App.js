b0ba778b35048e5ce0f705a1f70ff063dd21454a
sb7297 <sbaireddy7297@gmail.com>
2021-07-14 12:31:56 -0400

    App: fetch data from nasa api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import NasaMedia from "./components/NasaMedia";
 import Title from "./components/Title";
 
 function App() {
+  const [mediaUrl, setMediaUrl] = useState("");
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=bIUvX7E3fljMaeRzCp0Fp8HhHbLtIXnFJmTAE3p6&date=2021-03-03")
+    .then(res => {
+      setMediaUrl(res.url);
+    })
+    .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <Title />
