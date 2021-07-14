be8a1e16df297169888855afe007ac2fc43e01d3
sb7297 <sbaireddy7297@gmail.com>
2021-07-14 13:04:24 -0400

    render non-image media

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Title from "./components/Title";
 
 function App() {
   const [mediaUrl, setMediaUrl] = useState("");
+  const [mediaType, setMediaType] = useState("");
   const [date, setDate] = useState("");
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=bIUvX7E3fljMaeRzCp0Fp8HhHbLtIXnFJmTAE3p6")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=bIUvX7E3fljMaeRzCp0Fp8HhHbLtIXnFJmTAE3p6&date=2021-07-14")
     .then(res => {
       setMediaUrl(res.data.url);
       setDate(res.data.date);
+      setMediaType(res.data.media_type);
     })
     .catch(err => console.log(err));
   }, []);
   return (
     <div className="App">
       <Title date={date} />
-      <NasaMedia url={mediaUrl} />
+      <NasaMedia url={mediaUrl} type={mediaType} />
     </div>
   );
 }
--- src/components/NasaMedia.js
+++ src/components/NasaMedia.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function NasaMedia(props) {
-    const { url } = props;
+    const { url, type } = props;
     console.log(url);
-    const style = {
+    const imgStyle = {
         maxHeight: "84vh",
         maxWidth: "90%",
     };
-    return <img src={url} style={style} />;
+    const iframeStyle = {
+        width: "80%",
+        height: "84vh"
+    };
+    if (type === "image") return <img src={url} style={imgStyle} />;
+    return <iframe src={url} style={iframeStyle}></iframe>;
 }
 
 export default NasaMedia;
