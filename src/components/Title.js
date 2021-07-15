956db08130c75ab1e4b8d0add1ae4e0a5415edc1
sb7297 <sbaireddy7297@gmail.com>
2021-07-15 12:06:17 -0400

    Title: style with styled-components & a11y

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Arrow = styled.span`
+  cursor: pointer;
+`;
+
+const LeftArrowEmoji = () => <span role="img" aria-label="previous day">⬅️</span>
+const RightArrowEmoji = () => <span role="img" aria-label="next day">➡️</span>
 
 function Title({ date, decDate, incDate }) {
-  const arrowStyle = {
-    cursor: "pointer"
-  };
   return (
     <div className="titlebar">
       <h1>
-        <span style={arrowStyle} onClick={decDate}>⬅️ </span>
+        <Arrow onClick={decDate}><LeftArrowEmoji /> </Arrow>
         {date}
-        <span style={arrowStyle} onClick={incDate}> ➡️</span>
+        <Arrow onClick={incDate}> <RightArrowEmoji /></Arrow>
       </h1>
     </div>
   );
