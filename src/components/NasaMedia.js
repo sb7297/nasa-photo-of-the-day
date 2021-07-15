e904cc889653cff8ea0c899cbf2b9b7e8e4d5b45
sb7297 <sbaireddy7297@gmail.com>
2021-07-15 12:11:30 -0400

    NasaMedia: change indentation to 2 spaces

--- src/components/NasaMedia.js
+++ src/components/NasaMedia.js
@@ -2,19 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const NasaImg = styled.img`
-    max-height: 84vh;
-    max-width: 90%;
+	max-height: 84vh;
+	max-width: 90%;
 `;
 
 const NasaIframe = styled.iframe`
-    height: 84vh;
-    width: 80%;
+	height: 84vh;
+	width: 80%;
 `;
 
 function NasaMedia(props) {
-    const { url, type } = props;
-    if (type === "image") return <NasaImg src={url} alt="nasa astronomy photo of the day" />;
-    return <NasaIframe src={url} title="nasa astronomy photo of the day" />;
+	const { url, type } = props;
+	if (type === "image") return <NasaImg src={url} alt="nasa astronomy photo of the day" />;
+	return <NasaIframe src={url} title="nasa astronomy photo of the day" />;
 }
 
 export default NasaMedia;
